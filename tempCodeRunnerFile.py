79db7ca604713c413cf07928aa055c352b93ca7a
Shiban.M <shibanm23@gmail.com>
2023-05-28 20:24:11 +0530

    wrong

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,18 +1,3 @@
 
-            else:
-                flash('Cannot delete student record as they have books taken from the library')
-            return redirect('/reg_lib')
-        else:
-            error = 'Student record not found'
-            flash(error)
-            logging.warning(error)
-            return redirect('/reg_lib')
-    else:
-        error = 'You need to log in as librarian first.'
-        flash(error)
-        logging.warning(error)
-        return redirect('/librarian-login')
 
-
-
-#Route to open scanner
+#INDEX_ROUTE
